c0815054f1a5c55fd703431c0e40f651319070a0
clayandemar <clayandemar@gmail.com>
2023-02-08 19:08:14 -0600

    Initial commit

---
+++ Develop/models/index.js
@@ -0,0 +1,27 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+Products.belongsTo(Category, {
+  through: {
+    model: Tag,
+    unique: false
+  }
+})
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
